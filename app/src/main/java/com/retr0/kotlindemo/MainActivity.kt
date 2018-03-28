42b2968f2f4ce43315cdbf949f5d34f81505136e
Retr0 <amarok1217@163.com>
2018-03-28 16:55:02 +0800

    0328 1654

--- app/src/main/java/com/retr0/kotlindemo/MainActivity.kt
+++ app/src/main/java/com/retr0/kotlindemo/MainActivity.kt
@@ -1,21 +1,47 @@
-package com.retr0.kotlindemo
+package com.retr0.PingTool
 
+import android.os.AsyncTask
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Toast
+import com.retr0.PingTool.R
 import kotlinx.android.synthetic.main.activity_main.*
+import java.io.BufferedReader
+import java.io.InputStream
+import java.io.InputStreamReader
+import java.io.LineNumberReader
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
 
     val s: Int = 1
+    val address = "8.8.8.8"
 
     override fun onClick(v: View) {
 
         when (v) {
             bt_test -> {
                 Toast.makeText(this,"onclick",Toast.LENGTH_SHORT).show()
+
+                Thread({
+
+                    var process : Process = Runtime.getRuntime().exec("ping "+address)
+                    var imput : InputStream = process.inputStream
+                    var r : InputStreamReader = imput.reader()
+                    var bufferReader : BufferedReader = imput.bufferedReader()
+                    var msg : String= bufferReader.readLine()
+                    while ( msg != null){
+                        Log.e("ping",msg)
+                        msg = bufferReader.readLine()
+                    }
+
+                    
+                }).start()
+
+
+
             }
             else -> {
 
@@ -34,4 +60,8 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         Toast.makeText(this,"initView",Toast.LENGTH_SHORT).show()
         bt_test.setOnClickListener(this)
     }
+
+
+
+
 }
--- app/src/main/java/com/retr0/kotlindemo/Ping.kt
+++ app/src/main/java/com/retr0/kotlindemo/Ping.kt
@@ -3,3 +3,5 @@ package com.retr0.kotlindemo
 /**
  * Created by Retr0 on 2018/3/28 0028.
  */
+class Ping {
+}
